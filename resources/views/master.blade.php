ac55f7dc05ee0be8f228df19c794568078bbdc4e
Hugo St-Onge <hugostonge@gmail.com>
2019-04-19 21:26:12 -0400

    update header

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,16 +7,24 @@
         <style>
             body { font-family: Helvetica, sans-serif; }
             body * { box-sizing: border-box; }
+            .site-title { margin:0; float: left; position: relative; }
             .site-title small { color: grey; margin-left: 0.2em; }
+            .alpha { color: darkred; }
+            .gh-right { float:right; position: relative; }
             small { color: grey; }
+            header { clear:both; margin: 1em 0 2em 0; height: 35px; }
             footer { padding: 1em; color: grey; text-align: center; }
         </style>
         <script async defer src="https://buttons.github.io/buttons.js"></script>
         @stack('head')
     </head>
     <body>
-        <h1 class="site-title">Kodtrol<small>show control app</small></h1>
-        <p><a class="github-button" href="https://github.com/H-s-O/Kodtrol" data-size="large" aria-label="View H-s-O/Kodtrol on GitHub">View on GitHub</a></p>
+        <header>
+            <h1 class="site-title">Kodtrol<small>show control app <span class="alpha">[alpha]</span></small></h1>
+            <div class="gh-right">
+                <a class="github-button" href="https://github.com/H-s-O/Kodtrol" data-size="large" aria-label="View H-s-O/Kodtrol on GitHub">View on GitHub</a>
+            </div>
+        </header>
         @yield('content')
         <footer>
             A project by <a href="https://tekunika.ca" aria-label="TEKUNIKA" target="_blank">TEKUNIKA</a>
